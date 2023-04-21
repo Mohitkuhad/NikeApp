d26cd4571811f7284831dfb84a8dc788d4e54320
Mohit Kuhad <mohitkuhad8@gmail.com>
2023-04-22 00:44:11 +0530

    Navigation

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import ProductsScreen from "./src/screens/ProductsScreen";
-import ProductsDetailsScreen from "./src/screens/ProductsDetailsScreen";
-import ShoppingCart from "./src/screens/ShoppingCart";
+import Navigation from "./src/Navigation";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <ProductsScreen /> */}
-      {/* <ProductsDetailsScreen />*/}
-      <ShoppingCart />
+      <Navigation />
       <StatusBar style="auto" />
     </View>
   );
